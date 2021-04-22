5b98a16f6050b18019aae52e02a9acd210995233
Fermin Ordaz <fermin.ordaz@gmail.com>
2021-04-22 14:42:49 -0700

    Clean up run train wrapper

--- run_trainw.sh
+++ run_trainw.sh
@@ -1,7 +1,11 @@
-export MLFLOW_TRACKING_URI="postgresql://mlflowDbUser:demoable.secret.001@terraform-20210415031713058400000001.c7anop1qkfor.us-west-2.rds.amazonaws.com/mlflowDbStore"
+echo $MLFLOW_TRACKING_URI
 
 export PYTHONPATH=/home/ec2-user/workspace/fsdl-final-project
 
-conda activate pytorch_p36
+echo `id`
 
-python bin/run_train.py
+echo $PYTHONPATH
+
+# conda activate pytorch_p36
+
+# python bin/run_train.py
