e603a230a94eda01d86cc204225b29e2b35dcb70
Fermin Ordaz <fermin.ordaz@gmail.com>
2021-04-22 14:35:02 -0700

    Set conda and python path

--- run_trainw.sh
+++ run_trainw.sh
@@ -1,3 +1,7 @@
 export MLFLOW_TRACKING_URI="postgresql://mlflowDbUser:demoable.secret.001@terraform-20210415031713058400000001.c7anop1qkfor.us-west-2.rds.amazonaws.com/mlflowDbStore"
 
+export PYTHONPATH=/home/ec2-user/workspace/fsdl-final-project
+
+conda activate pytorch_p36
+
 python bin/run_train.py
