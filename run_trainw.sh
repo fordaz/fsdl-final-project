a623c1e6b7b6180a8c2fb80600bfa00f58e1eff8
Fermin Ordaz <fermin.ordaz@gmail.com>
2021-04-22 15:34:14 -0700

    Put conda activate back

--- run_trainw.sh
+++ run_trainw.sh
@@ -1,13 +1,17 @@
-echo $MLFLOW_TRACKING_URI
+echo "Using mlflow tracking url $MLFLOW_TRACKING_URI"
+echo "Using python path $PYTHONPATH"
 
-export PYTHONPATH=/home/ec2-user/workspace/fsdl-final-project
-
-echo `id`
-
-echo $PYTHONPATH
-
+# setting up conda
 source /home/ec2-user/anaconda3/etc/profile.d/conda.sh
 
+# activating the aws instance conda environment ready for DL
 conda activate pytorch_p36
 
+# installing the application dependencies
+pip install -r requirements.txt
+
+# pulling the training datasets
+dvc pull
+
+# launching the training
 python bin/run_train.py
